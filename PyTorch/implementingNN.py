1d83ce59e6ad31ace4f60b78a8d56339d90004d2
Sharad-Banga <sharadiit03@gmail.com>
2026-07-05 23:29:02 +0530

    implementing NN

---
+++ PyTorch/implementingNN.py
@@ -0,0 +1,27 @@
+import torch
+class NeuralNetwork(torch.nn.Module):
+  def __init__(self , num_inputs,num_outputs):
+    super().__init__()
+
+    # Sequential is a container that stacks layers in order.
+    self.layers = torch.nn.Sequential(
+
+      # 1st hidden layer
+      torch.nn.Linear(num_inputs , 30),
+      torch.nn.ReLU(),
+
+      # 2nd hidden layer
+      torch.nn.Linear(30 , 20),
+      torch.nn.ReLU(),
+
+      # output layer
+      torch.nn.Linear(20 ,num_outputs)
+    )
+
+  def forward(self , x):
+    logits = self.layers(x)
+    return logits
+    
+model = NeuralNetwork(4,2);
+# print(model);
+print(model.layers[0].weight)
